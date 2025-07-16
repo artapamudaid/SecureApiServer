828458d22a261722ab1e13406a7a3113780721bd
Arta Tri Pamuda <pamuda.id@gmail.com>
2025-07-16 10:07:13 +0700

    update routes

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,12 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Artapamudaid\SecureApiServer\Http\Middleware\ValidateApiKey;
+use Artapamudaid\SecureApiServer\Http\Controllers\ApiKeyController;
 
-Route::middleware(['api', ValidateApiKey::class])
-    ->prefix('secure-api')
-    ->group(function () {
-        Route::post('/ping', function () {
-            return response()->json(['pong' => true]);
-        });
+Route::prefix('secure-api')->group(function () {
+    Route::post('/key', [ApiKeyController::class, 'generate']);
+    Route::get('/keys', [ApiKeyController::class, 'index']);
+    Route::delete('/key/{id}', [ApiKeyController::class, 'destroy']);
+    Route::patch('/key/{id}/revoke', [ApiKeyController::class, 'revoke']);
+
+    // Example secured ping endpoint
+    Route::middleware('secure-api')->post('/ping', function () {
+        return response()->json(['message' => 'pong']);
     });
+});
